63612ea7463dfb3c2623b2c5c41fecb60785dddf
SantaCruz77 <steveyamada66@gmail.com>
2023-01-22 18:25:07 +0900

    incorporating-designs-using-templates

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,9 +1,28 @@
 @include("parts.common", ["title" => "会社概要"])
-@include("parts.header")
-<main>
-    <h1>会社概要ページ</h1>
-    <p>現在の時間は {{ $current_time }} です</p>
-</main>
+@include("parts.nav")
+
+<!-- Image Showcases-->
+<section class="showcase">
+    <div class="container-fluid p-0">
+        <div class="row no-gutters">
+            <div class="col-lg-6 order-lg-1 my-auto showcase-text">
+                <h2>会社概要ページ</h2>
+                
+                <p class="lead mb-0">
+                    株式会社 CreateSystemsの会社概要についてお知らせします。<br />
+                    現在の時間は {{ $current_time }} です<br />
+                    <br />
+                    
+                    <ul>
+                        <li>社　名：　株式会社 CreateSystems</li>
+                        <li>創　業：　2022年10月1日</li>
+                        <li>代表者：　妻夫木　聡</li>
+                        <li>所在地：　東京都千代田区日本橋1−1−1</li>
+                    </ul>
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
 @include("parts.footer")
-</body>
-</html>
--- resources/views/parts/nav.blade.php
+++ resources/views/parts/nav.blade.php
@@ -1,4 +1,6 @@
-<nav>
-    <a href="{{ url('/') }}">トップ</a>
-    <a href="{{ url('/about') }}">会社概要</a>
+<nav class="navbar navbar-light bg-light static-top">
+    <div class="container">
+        <a id="company-name" class="navber-brand" href="{{ url('/') }}">株式会社 CreateSystems</a>
+        <a class="btn btn-primary" href="{{ url('/about') }}">会社概要</a>
+    </div>
 </nav>
