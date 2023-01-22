6752d5181807e2d29dea15e2efc44365294c7d40
SantaCruz77 <steveyamada66@gmail.com>
2023-01-22 11:00:30 +0900

    complete development top-page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel (ﾟ∀ﾟ)
                 </div>
 
                 <div class="links">
